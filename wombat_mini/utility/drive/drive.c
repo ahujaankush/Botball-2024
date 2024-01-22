0d34a2e17c5b6343c86699a0e40b9b42144f0fc2
Ankush Ahuja <aahuja06@proton.me>
2024-01-22 13:37:27 +0100

    Temp, repo will be removed after transfer

--- wombat_mini/utility/drive/drive.c
+++ wombat_mini/utility/drive/drive.c
@@ -1,16 +1,17 @@
 #include "../../types.h"
 #include <stdbool.h>
 
-const int correctionConst = 0;
+// correction constant used for forward drive (can also be used for backw. obv.)
+const int correctionConst = 3;
 
 void forward(int speed, bool newInit) {
-  // Error produced
-  short currentGyroVal = gyro_x();
-  static short initGyroVal = -1;
+  // get current rotation
+  short currentGyroVal = gyro_z();
+  // @NOTE: will reuse value when recalled
+  static short initGyroVal = 0;
   if (newInit) {
     initGyroVal = currentGyroVal;
   }
-
   int generalError = correctionConst * (initGyroVal - currentGyroVal);
   int generalPower = (M_MAX_SPEED * speed) / 100;
 
@@ -23,9 +24,14 @@ void forward(int speed, bool newInit) {
 
 void backwards(int speed, bool newInit) { forward(-speed, newInit); }
 
+void left(int d) {
 
-void left(int d) {}
+}
 
-void right(int d) {}
+void right(int d) {
 
-void follow_line() {}
+}
+
+void follow_line() {
+
+}
